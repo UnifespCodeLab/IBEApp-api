09e7a3dfd08797207a4c8899e602f19e2958c955
VinicGou <sonic.viniciusmg@gmail.com>
2025-10-13 17:54:44 -0300

    Pequenas correções no novo formato

--- src/api/domain/User.py
+++ src/api/domain/User.py
@@ -1,14 +1,15 @@
 from typing import Optional, Any, Dict
 from pydantic import Field, EmailStr
 from .Base import MongoModel 
+from .Base import UserRole  
 
 class Usuario(MongoModel):
     username: str = Field(..., max_length=50)
     email: EmailStr
     password: str
     name: str = Field(..., max_length=100)
+    role: UserRole = Field(default=UserRole.USER)
     avatar_url: Optional[str] = Field(None, alias="avatarUrl")
     instituicao: Dict[str, Any] = Field(default_factory=dict)
     terms_accepted: bool = Field(..., alias="termsAccepted")
     profile_data: Dict[str, Any] = Field(default_factory=dict, alias="profileData")
-    role: str = Field(default="user")
